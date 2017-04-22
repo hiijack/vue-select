73c3c5e6a7d978d0d3832770bf7cd2f2563a8db1
lunjz <jackerlun@gmail.com>
2017-04-22 09:34:56 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CustomSelect></CustomSelect>
+  </div>
+</template>
+
+<script>
+import CustomSelect from './components/CustomSelect'
+
+export default {
+  name: 'app',
+  components: {
+    CustomSelect
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
